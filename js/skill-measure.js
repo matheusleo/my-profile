279339ed02093742240bc965bd85c7a5413a7b58
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-20 20:30:07 -0300

    Planejar codigo da animaçao
    Planeja o codigo em JS da animaçao que sera implementada sobre a barra. Ela permitira que ao passar o mouse por cima (hover) haja uma mundaça no tamanho da barra e, ao retirar, a barra ira novamente contrair

---
+++ js/skill-measure.js
@@ -0,0 +1,22 @@
+//Plano A: Criar animação da barra com JS
+// 3 momentos
+//  a) barra carregada pela página -  value="0" no momento inicial resolve o problema
+//  b) hover na barra -
+//    0. Localiar a barra
+//    1. Selecionar a barra
+//    2. Adicionar um eventListener para o hover
+//    3. Modificar o value da barra após o hover
+//
+//  c) saída do hover
+//    0. Localiar a barra
+//    1. Selecionar a barra
+//    2. Adicionar um eventListener para a saída do mouse
+//    3. Modificar o value da barra após o saída do mouse
+//
+//Plano B: Criar JS com objetos que implementa o código repetido
+// 3 momentos
+//  a) Cria objeto com os seguintes dados {skill, imagem da skill, valor da skill}
+//  b) Cria a função que irá de percorrer o objeto colhendo as informações, montando o código e implementado
+//  c) Ao carregar a pagina chama função que executa a repetição
+//
+//Plano C: Implementar juntos o Plano A e B
