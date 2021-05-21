eea7dbffd02b517fdb939a6b0f71c2cc5780c04e
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-20 21:28:34 -0300

    Criar animaçao da barra
    Utilizando o JavaScript, foi criada a animaçao da barra para que, quando o mouse passe por cima, haja uma mudança no skill level e na saida tambem

--- js/skill-measure.js
+++ js/skill-measure.js
@@ -2,16 +2,18 @@
 // 3 momentos
 //  a) barra carregada pela página -  value="0" no momento inicial resolve o problema
 //  b) hover na barra -
-//    0. Localiar a barra
-//    1. Selecionar a barra
-//    2. Adicionar um eventListener para o hover
-//    3. Modificar o value da barra após o hover
+//    0. Localiar a barra [ok]
+//    1. Selecionar a barra [ok]
+//    2. Adicionar um eventListener para o hover [ok]
+//    3.1 Modificar o value da barra após o hover [ok]
+//    3.2 Modificar a porcentagem dentro do meter após o hover [ok]
 //
 //  c) saída do hover
-//    0. Localiar a barra
-//    1. Selecionar a barra
-//    2. Adicionar um eventListener para a saída do mouse
-//    3. Modificar o value da barra após o saída do mouse
+//    0. Localiar a barra [ok]
+//    1. Selecionar a barra [ok]
+//    2. Adicionar um eventListener para a saída do mouse [ok]
+//    3. Modificar o value da barra após o saída do mouse [ok]
+//    3.2 Modificar a porcentagem dentro do meter após a saída [ok]
 //
 //Plano B: Criar JS com objetos que implementa o código repetido
 // 3 momentos
@@ -20,3 +22,13 @@
 //  c) Ao carregar a pagina chama função que executa a repetição
 //
 //Plano C: Implementar juntos o Plano A e B
+
+const skillBar = document.querySelector(".measure");
+skillBar.addEventListener("mouseover", () => {
+  skillBar.value = 50;
+  skillBar.textContent = "50%";
+});
+skillBar.addEventListener("mouseout", () => {
+  skillBar.value = 0;
+  skillBar.textContent = "0%";
+});
