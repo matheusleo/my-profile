eeed169dcc313d60f55e04cf819e695e9821d55d
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-20 21:55:08 -0300

    Criar novo arquivo JS
    Cria novo arquivo JS para que, ao inves de repetir o codigo HTML, ele seja criado dinamicamente. Criado um objeto com as 5 skills a serem adicionadas

---
+++ js/skill-itens.js
@@ -0,0 +1,41 @@
+//Plano B: Criar JS com objetos que implementa o código repetido
+// 3 momentos
+//  a) Cria objeto com os seguintes dados {skill, imagem da skill, valor da skill}
+//  b) Cria a função que irá de percorrer o objeto colhendo as informações, montando o código e implementado
+//  c) Ao carregar a pagina chama função que executa a repetição
+//
+//Plano C: Implementar juntos o Plano A e B
+//Plano D: Implementar para que o Plano A funcione com focus
+
+const mySkills = {
+  html5: {
+    skillIcon:
+      "https://github.com/matheusleo/my-profile/blob/section-skills/assets/img/html5.png?raw=true",
+    skillTitle: "HTML5",
+    skillValue: "60",
+  },
+  css3: {
+    skillIcon:
+      "https://github.com/matheusleo/my-profile/blob/section-skills/assets/img/css3.png?raw=true",
+    skillTitle: "CSS3",
+    skillValue: "50",
+  },
+  js: {
+    skillIcon:
+      "https://github.com/matheusleo/my-profile/blob/section-skills/assets/img/js.png?raw=true",
+    skillTitle: "JavaScript",
+    skillValue: "40",
+  },
+  react: {
+    skillIcon:
+      "https://github.com/matheusleo/my-profile/blob/section-skills/assets/img/react.png?raw=true",
+    skillTitle: "React",
+    skillValue: "10",
+  },
+  UX: {
+    skillIcon:
+      "https://github.com/matheusleo/my-profile/blob/section-skills/assets/img/ux.png?raw=true",
+    skillTitle: "UX",
+    skillValue: "10",
+  },
+};
--- js/skill-measure.js
+++ js/skill-measure.js
@@ -14,14 +14,6 @@
 //    2. Adicionar um eventListener para a saída do mouse [ok]
 //    3. Modificar o value da barra após o saída do mouse [ok]
 //    3.2 Modificar a porcentagem dentro do meter após a saída [ok]
-//
-//Plano B: Criar JS com objetos que implementa o código repetido
-// 3 momentos
-//  a) Cria objeto com os seguintes dados {skill, imagem da skill, valor da skill}
-//  b) Cria a função que irá de percorrer o objeto colhendo as informações, montando o código e implementado
-//  c) Ao carregar a pagina chama função que executa a repetição
-//
-//Plano C: Implementar juntos o Plano A e B
 
 const skillBar = document.querySelector(".measure");
 skillBar.addEventListener("mouseover", () => {
