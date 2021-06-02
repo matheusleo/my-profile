abc4e5d33fb92ff3aa85be63abe7cb296c9b4f0f
Matheus Leonardo <82543651+matheusleo@users.noreply.github.com>
2021-06-02 16:05:47 -0300

    Revert "Animation Section Description"

--- js/project-cards.js
+++ js/project-cards.js
@@ -1,41 +1,38 @@
-const sectionDescription = document.querySelector("#section-description");
+const listProjects = document.querySelector("#projects-list");
 
 const myProjects = new Map([
   [
     "AluraFlix",
     {
       projectLink: "https://matheusleo.github.io/AluraFlix/",
+      padTop: 52.225,
       projectImage:
         "https://github.com/matheusleo/my-profile/blob/master/assets/img/aluraflix.png?raw=true",
       projectTitle: "AluraFlix",
-      projectDescription:
-        "O AluraFlix foi o primeiro projeto e mais simples que eu desenvolvi utilizando HTML, CSS e JavaScript acompanhando as lives de programação na twitch do MarcoBrunoDev. Seu conceito foi elaborado durante a Imersão Dev da Alura, em 2021.",
-      projectBg: 1,
     },
   ],
   [
     "Curtas.tv",
     {
       projectLink: "https://squad-08.github.io/curtas.tv/",
+      padTop: 14,
       projectImage:
         "https://github.com/matheusleo/my-profile/blob/master/assets/img/curtas.tv.png?raw=true",
-      projectTitle: "CurtasTv",
-      projectDescription:
-        "O conceito veio de criar uma plataforma na qual, ao invés de precisar procurar curta por curta no Youtube que é uma plataforma com milhões de vídeos, o usuário poderia ter fácil acesso a uma coletânea de curtas, separados por gênero. Assim, surgiu o Curtas Tv.",
-      projectBg: 0,
+      projectTitle: "Curtas.tv",
     },
   ],
 ]);
 
 function createProjectCard(project) {
   const projectCard = `
-    <button id=${project.projectTitle} class="project-card">
-      <img src=${project.projectImage} alt="Logo do projeto ${project.projectTitle}" class="project">
-      <div class="project-overlay">Conhecer mais sobre</div>
-    </button>
+    <article class="project-card">
+        <a href=${project.projectLink} rel="noreferrer noopener" target="_blank">
+            <img style="padding-top: ${project.padTop}px;" src=${project.projectImage} alt="Logo do projeto ${project.projectTitle}" class="project">
+        </a>
+    </article>
     `;
 
-  sectionDescription.insertAdjacentHTML("beforebegin", projectCard);
+  listProjects.insertAdjacentHTML("beforeend", projectCard);
 }
 
 myProjects.forEach(createProjectCard);
