42a2b68601e52a158575e0c256fad86085d141e8
Matheus Leonardo <matheusleotrab@gmail.com>
2021-06-04 13:57:06 -0300

    Corrigir fonte (3/3)
    Corrigir src das imagens dos prject cards

--- js/project-cards.js
+++ js/project-cards.js
@@ -6,7 +6,7 @@ const myProjects = new Map([
     {
       projectLink: "https://matheusleo.github.io/AluraFlix/",
       projectImage:
-        "https://github.com/matheusleo/my-profile/blob/master/assets/img/aluraflix.png?raw=true",
+        "https://github.com/matheusleo/my-profile/blob/main/assets/img/aluraflix.png?raw=true",
       projectTitle: "AluraFlix",
       projectDescription:
         "O AluraFlix foi o primeiro projeto e mais simples que eu desenvolvi utilizando HTML, CSS e JavaScript acompanhando as lives de programação na twitch do MarcoBrunoDev. Seu conceito foi elaborado durante a Imersão Dev da Alura, em 2021.",
@@ -18,7 +18,7 @@ const myProjects = new Map([
     {
       projectLink: "https://squad-08.github.io/curtas.tv/",
       projectImage:
-        "https://github.com/matheusleo/my-profile/blob/master/assets/img/curtas.tv.png?raw=true",
+        "https://github.com/matheusleo/my-profile/blob/main/assets/img/curtas.tv.png?raw=true",
       projectTitle: "CurtasTv",
       projectDescription:
         "O conceito veio de criar uma plataforma na qual, ao invés de precisar procurar curta por curta no Youtube que é uma plataforma com milhões de vídeos, o usuário poderia ter fácil acesso a uma coletânea de curtas, separados por gênero. Assim, surgiu o Curtas Tv.",
