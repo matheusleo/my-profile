3e8135f8141ba6c5bf1adab840127ee1d1ef542e
Matheus Leonardo <matheusleotrab@gmail.com>
2021-06-11 11:51:11 -0300

    Modificar menu-lateral
    Modifica o menu lateral de tal forma que ao clicar em qualquer opçao do menu ele fecha apos determinado tempo - ira ser modificado e definido apos implementar a transiçao de sections - mas, ao clicar em um espaço sem contudo isso nao ocorre

--- js/menu-control.js
+++ js/menu-control.js
@@ -1,20 +1,15 @@
-// O que eu quero?
-// Plano A
-//      Eu quero que, ao clicar no menu-control, o menu lateral seja aberto da
-// direita para a esquerda e o usuário possa ter acesso a ele e selecionar as opções dele.
-//
-// Plano B
-//      Após selecionar uma opção do menu, o menu lateral deve ser fechado
-//
-// Passo A Passo - Plano A
-//      1. Selecionar o "menu-control" [ok]
-//      2. Adicionar eventListener ao "menu-control" [ok]
-//      3. Criar a variação de classe active para o menu que é ativada ao clicar [ok]
-
 const menuControl = document.querySelector("#menu-control");
 const navMenu = document.querySelector("#menu-nav");
+const optionsMenu = document.querySelector("#menu-options");
 
-menuControl.addEventListener("click", () => {
+function changeMenu() {
   menuControl.classList.toggle("-active");
   navMenu.classList.toggle("-active");
+}
+
+menuControl.addEventListener("click", changeMenu);
+
+optionsMenu.addEventListener("click", () => {
+  setTimeout(changeMenu, 150);
+  //modificar o tempo quando descobrir como funciona a transição entre sections
 });
