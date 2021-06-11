6499c7b7a8c4a75e3bb41154d04e4c03341e3f2b
Matheus Leonardo <matheusleotrab@gmail.com>
2021-06-11 19:41:28 -0300

    Modificar menu-nav
    Adiciona logica em JS para que, ao abrir o menu-nav, o fundo fique com blur e, ao sair dele, o menu perca o blur

--- js/menu-control.js
+++ js/menu-control.js
@@ -1,10 +1,17 @@
 const menuControl = document.querySelector("#menu-control");
 const navMenu = document.querySelector("#menu-nav");
 const optionsMenu = document.querySelector("#menu-options");
+const mainContent = document.querySelector("#main-content");
 
 function changeMenu() {
-  menuControl.classList.toggle("-active");
+  const menuState = menuControl.classList.toggle("-active");
   navMenu.classList.toggle("-active");
+
+  if (menuState) {
+    mainContent.style.filter = "blur(.25em)";
+  } else {
+    mainContent.style.filter = "none";
+  }
 }
 
 menuControl.addEventListener("click", changeMenu);
