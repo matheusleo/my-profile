8ba4802582ea1a221ccf31dc76abe9817d715bfa
Matheus Leonardo <matheusleotrab@gmail.com>
2021-06-21 21:50:13 -0300

    Modificar arquivo about-card.js
    Cria uma nova feature responsavel pela geraçao dinamica do contudo da timeline - que sao os about-cards - a partir do antigo arquivo about-card.js

--- js/about-cards.js
+++ js/about-cards.js
@@ -1,27 +1,46 @@
-// Qual a ideia?:
-// A ideia é que haja botões que permitam a mudança de um card para o outro.
-// Ao clicar em um button, é preciso que card troque.
-//
-// Passo A Passo
-//      1. Adicionar eventListener aos buttons
-//      2. Utilizar o scrollBy para gerar o scroll no wrapper-cards
-
 const wrapperCards = document.querySelector("#wrapper-cards");
-const buttonArrowLeft = document.querySelector("#button-arrow-left");
-const buttonArrowRight = document.querySelector("#button-arrow-right");
+const aboutContent = new Map([
+  [
+    "pitágoras",
+    {
+      aboutYear: 2006,
+      aboutImage:
+        "https://raw.githubusercontent.com/matheusleo/my-profile/3b6094390e442a122f8a79860cc94a3891c169e8/assets/img/logo-pitagoras-colored.svg",
+      aboutAlt: "Logo do Colégio Pitágoras - Cidade Jardim",
+      aboutTitle: "Colégio Pitágoras",
+      aboutDescription:
+        "Colégio no qual tive a alegria de estudar desde 2006 e de me formar em 2019 - ano em que realizei o ENEM.",
+    },
+  ],
+  [
+    "UFMG",
+    {
+      aboutYear: 2020,
+      aboutImage:
+        "https://github.com/matheusleo/my-profile/blob/section-about/assets/img/ufmg-2.png?raw=true",
+      aboutAlt: "Logo da Universidade Federal de Minas Gerais",
+      aboutTitle: "UFMG",
+      aboutDescription:
+        "Na primeira tentativa, em 2019, passei na renomada UFMG, em Física, para começar meus estudos em 2020.",
+    },
+  ],
+]);
 
-buttonArrowLeft.addEventListener("click", () => {
-  wrapperCards.scrollBy({
-    top: 0,
-    left: -351,
-    behavior: "smooth",
-  });
-});
+function contentTimeline(aboutCard) {
+  const aboutCardCode = `
+    <article class="about-card" tabindex="0">
+        <time class="subtitle-main -year" datetime="${aboutCard.aboutYear}">${aboutCard.aboutYear}</time>
+        <div class="about-wrapper">
+            <img src=${aboutCard.aboutImage} 
+                alt=${aboutCard.aboutAlt}
+                class="about-image">
+        </div>
+        <h3 class="title-main -about -long-name">${aboutCard.aboutTitle}</h3>
+        <p class="description-paragraph -about">${aboutCard.aboutDescription}
+    </article>
+    `;
 
-buttonArrowRight.addEventListener("click", () => {
-  wrapperCards.scrollBy({
-    top: 0,
-    left: 351,
-    behavior: "smooth",
-  });
-});
+  wrapperCards.insertAdjacentHTML("beforeend", aboutCardCode);
+}
+
+aboutContent.forEach(contentTimeline);
